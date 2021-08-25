970bde77e160fa5f31c32ca7d74f97ac74e26007
Mahmoud Alsamman <acc.ma7moud@gmail.com>
2021-08-25 22:24:27 +0200

    Create Application factory.

---
+++ MovieDB/MovieDB/Application/ApplicationFactory.swift
@@ -0,0 +1,16 @@
+//
+//  ApplicationFactory.swift
+//  MovieDB
+//
+//  Created by Mahmoud Alsamman on 25/08/2021.
+//
+
+import Foundation
+
+/// Class that stores all dependencies used by the app.
+fileprivate final class ApplicationDependencies { }
+
+/// Factory class that creates view controllers and injects them with proper dependencies.
+final class ApplicationFactory {
+    fileprivate let dependencies = ApplicationDependencies()
+}
