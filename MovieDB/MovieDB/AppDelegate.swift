66a5827cf9cba3c81aa60d4742a043b2794abdef
Mahmoud Alsamman <acc.ma7moud@gmail.com>
2021-08-25 21:43:54 +0200

    Initiate App.

---
+++ MovieDB/MovieDB/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovieDB
+//
+//  Created by Mahmoud Alsamman on 25/08/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        return true
+    }
+}
