411cd98b6ac8baa534c0e955d5d86a890953f3d9
Mahmoud Alsamman <acc.ma7moud@gmail.com>
2021-08-25 22:59:09 +0200

    Create TableView.

---
+++ MovieDB/MovieDB/Screens/SearchList/MovieCellView.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCellView.swift
+//  MovieDB
+//
+//  Created by Mahmoud Alsamman on 25/08/2021.
+//
+
+import UIKit
+
+final class MovieCellView: UITableViewCell {
+    static let id = "MovieCellView"
+}
--- MovieDB/MovieDB/Screens/SearchList/SearchListView.swift
+++ MovieDB/MovieDB/Screens/SearchList/SearchListView.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 final class SearchListViewView: CodeView {
+    
+    private lazy var tableView: UITableView = {
+        let tb = UITableView()
+        tb.translatesAutoresizingMaskIntoConstraints = false
+        tb.register(MovieCellView.self, forCellReuseIdentifier: MovieCellView.id)
+        tb.rowHeight = 70
+        tb.backgroundColor = .clear
+        tb.showsVerticalScrollIndicator = false
+        return tb
+    }()
+    
+    
+    
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+    }
+}
+
+extension SearchListViewView: ViewSetupable {
+    func setupViewHierarchy() {
+        
+    }
+    
+    func setupConstraints() {
+        
     }
 }
